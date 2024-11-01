7b062a7956bbc9ef4628a02a59b17d924607c0cf
Ulices98 <145692868+JavierUlices@users.noreply.github.com>
2024-10-31 20:40:42 -0600

    Add files via upload

---
+++ Ejercicios_funciones/Ejercicio4.js
@@ -0,0 +1,6 @@
+// 4. Crea una función que reciba un array de strings y devuelva un nuevo array con las strings en mayúsculas//
+// Javier Eulices Martínez Martínez
+function toMayusculas(arr) {
+    return arr.map(string => string.toUpperCase());
+    }
+console.log(toMayusculas(["hola, mundo"]));
